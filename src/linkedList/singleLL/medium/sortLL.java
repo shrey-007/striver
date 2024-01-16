4e618f73d22bf2562807c9a50e24c903fb44364e
shreySaxena <shreyyerhs07@gmail.com>
2024-01-16 21:48:53 +0530

    added ques

--- src/linkedList/singleLL/medium/sortLL.java
+++ src/linkedList/singleLL/medium/sortLL.java
@@ -34,6 +34,52 @@ public class sortLL {
         }
         return head;
     }
+
+
+//    method2=using merge sort
+      public static Node getMiddleNode(Node head){
+        //generally we start fast and slow from head itself, but agar ll ka size even hai toh slow n/2 na dekar n/2+1 deta hai
+        // ex 1,2,3,4,5,6 isme agar fast and slow dono head se start kroge toh slow 4 ko point krega.
+        //but middle 3 hona chaiye, islie fast ko ek jyaada se start kra
+        Node fast=head.next;
+        Node slow=head;
+        while(fast!=null || fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+      }
+
+      //method2=merge sort
+      public static Node func2(Node head){
+        /*
+         imagine kro ki ye array pr lagani hai merge sort toh kya kya loge
+         1)array<->ll = jo ki head se mil jaaegi
+         2)mid element=jo ki function se mil jaaega
+         3)start and end nhi lenge isme(start and end ka main aim ye tha ki aaray kese do part mai divide hoga, yaha
+           apan ne middleNode  se hi decide kr liya kese divide hoga)
+        * */
+          if(head==null || head.next==null){
+              //so ll contains 1 or 0 element means it is already sorted
+              return head;
+          }
+          //you got the middle node of ll
+          Node middleNode=getMiddleNode(head);
+
+          //now break the ll into two parts, but break krne se pehle dono ke head store krlo
+          Node leftHead=head;  //start
+          Node rightHead=middleNode.next;  //mid+1
+          //break ll into 2 parts
+          middleNode.next=null;
+
+          //sort the left ll
+          Node leftSorted=func2(head);
+          //sort the right ll
+          Node rightSorted=func2(rightHead);
+          //now we have 2 sorted ll, merge them
+          return MergeTwoSortedLL.func(leftSorted,rightSorted);
+      }
+
     public static void main(String[] args) {
 
     }
